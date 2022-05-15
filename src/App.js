4db13bd6622760cabcbe0f945ecd00fd517d4656
sami <sami.akriche@esprit.tn>
2022-05-15 21:18:10 +0200

    seconde commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import React, { Component }  from 'react';
 import Create from './components/create-client';
 import Read from './components/read-client';
 //import Update from './components/update';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route ,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className="main">
         <h2 className="main-header">React Crud Operations</h2>
-        <div>
-          <Route exact path='/create-client' component={Create} />
-        </div>
-        <div style={{ marginTop: 20 }}>
+        <Routes>
+        
+          
+        
+        
           <Route exact path='/read-client' component={Read} />
-        </div>
+          <Route path="/" element={<div>Default Page Content</div>} />
+          <Route path="/create-client" element={<Create/>} />  
+        
+        </Routes>
 
         
       </div>
